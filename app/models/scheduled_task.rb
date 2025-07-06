fd8ed33faaea5a46ecf90b89462ddbd24b7c0ef8
Billy Monk <billy.monk@gmail.com>
2025-07-06 00:52:39 -0400

    Add uniqueness validation for ScheduledTask

--- app/models/scheduled_task.rb
+++ app/models/scheduled_task.rb
@@ -1,4 +1,6 @@
 class ScheduledTask < ApplicationRecord
   belongs_to :task
   belongs_to :timeslot
+
+  validates :task_id, uniqueness: { scope: :timeslot_id, message: "already assigned to this timeslot" }
 end
